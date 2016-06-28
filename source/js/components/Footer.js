bc2c44a1f6b1ebfbfac1239dcb02c212e5185ebd
Matt States <mattstates@icloud.com>
2016-06-28 00:48:24 -0700

    spelling

--- source/js/components/Footer.js
+++ source/js/components/Footer.js
@@ -3,7 +3,9 @@ import './footer.sass'
 
 const Footer = function() {
   return (
-    <div className="footer">Created by: <a href="http://www.mattstates.com">Matt States</a> See the Source Code at <a href="http://www.github.com/mattstates/roguelike">github.com</a></div>
+    <div className="footer">Created by: <a href="http://www.mattstates.com" target=
+    "_blank">Matt States</a> See the Source Code at <a href="http://www.github.com/mattstates/roguelike" target=
+    "_blank">github.com</a></div>
   )
 }
 
